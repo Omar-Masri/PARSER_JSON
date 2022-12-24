4b0095d1e368aa4b891e20317dc6ab00811e591e
Lorenzo Piazza <lorenzopiazza02@gmail.com>
2022-12-24 10:30:16 +0100

    Initial input/output for lisp jsonparse, jsonread working but underperforming, jsondump to continue

--- Lisp/jsonparse.lisp
+++ Lisp/jsonparse.lisp
@@ -1,6 +1,25 @@
 ;;;; -*- Mode: Lisp -*-
 ;;; -- jsonparse.lisp --
 
-(defun json_ws s (cond (eq (char s 0)  ) ))
+;;; ----- input e output -----
+
+(defun jsonread (filename)
+  (with-open-file (in 
+                   filename 
+                   :direction :input 
+                   :if-does-not-exist :error)
+   (readfile in "")))
+
+(defun readfile (in res-string) 
+  (let ((s (read-char in nil 'eof)))
+     (if (eq s 'eof) (print res-string) (readfile in (concatenate 'string res-string (list s))))))
+
+(defun jsondump (json filename)
+  (with-open-file (out 
+                   filename
+                   :direction :output
+                   :if-exists :supersede
+                   :if-does-not-exist :create)
+(format out json)))
 
 ;;; end of file -- jsonparse.lisp --
