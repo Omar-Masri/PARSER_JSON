5a11da1225f8e5c2a3729bf6167c79c43dc218c1
Lorenzo Piazza <lorenzopiazza02@gmail.com>
2022-12-24 10:43:30 +0100

    Minor commit - Add some comments and minor changes

--- Lisp/jsonparse.lisp
+++ Lisp/jsonparse.lisp
@@ -3,6 +3,12 @@
 
 ;;; ----- input e output -----
 
+
+;;; jsonread/1
+;;; jsonread(filename)
+;;; read json file and print to screen for now
+;;; call jsonparse where is the print in readfile
+
 (defun jsonread (filename)
   (with-open-file (in 
                    filename 
@@ -12,7 +18,15 @@
 
 (defun readfile (in res-string) 
   (let ((s (read-char in nil 'eof)))
-     (if (eq s 'eof) (print res-string) (readfile in (concatenate 'string res-string (list s))))))
+     (if (eq s 'eof) (print res-string) 
+       (readfile in (concatenate 'string res-string (list s))))))
+
+
+;;; jsondump/1
+;;; jsondump(JSON filename)
+;;; write the JSON passed into the file specified by the path filename
+;;; (IMPORTANT: ASK FOR EXAMPLE)
+;;; need implement the revert conversion from jsonobj to true json
 
 (defun jsondump (json filename)
   (with-open-file (out 
